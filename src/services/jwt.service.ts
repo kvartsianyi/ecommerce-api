47f282cda01e647cdcbf2097eb1060d50d09b4a3
Yuriy <liparg104@gmail.com>
2025-07-17 19:41:41 +0300

    feat: add user activation endpoint

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,7 +1,7 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 import { TOKEN_CONFIG_MAP } from '@/config';
-import { DualTokenActions, SingleTokenActions, SingleTokenConfig, TokenPair } from '@/models';
+import { DualTokenActions, SingleTokenActions, TokenConfig, TokenPair } from '@/models';
 
 class JwtService {
   async generateTokenPair(
@@ -10,27 +10,48 @@ class JwtService {
   ): Promise<TokenPair> {
     const tokenPairConfig = TOKEN_CONFIG_MAP[action];
 
-    const accessToken = await this.generateToken(tokenPairConfig.ACCESS_TOKEN, payload);
-    const refreshToken = await this.generateToken(tokenPairConfig.REFRESH_TOKEN, payload);
+    const accessToken = await this.generateToken(payload, tokenPairConfig.ACCESS_TOKEN);
+    const refreshToken = await this.generateToken(payload, tokenPairConfig.REFRESH_TOKEN);
 
     return { accessToken, refreshToken };
   }
 
+  async generateToken(payload: JwtPayload, action: SingleTokenActions): Promise<string>;
+  async generateToken(payload: JwtPayload, config: TokenConfig): Promise<string>;
   async generateToken(
-    action: SingleTokenActions,
-    payload: Record<string, unknown>,
-  ): Promise<string>;
-  async generateToken(config: SingleTokenConfig, payload: Record<string, unknown>): Promise<string>;
-  async generateToken(
-    actionOrConfig: SingleTokenActions | SingleTokenConfig,
-    payload: Record<string, unknown>,
+    payload: JwtPayload,
+    actionOrConfig: SingleTokenActions | TokenConfig,
   ): Promise<string> {
-    const tokenConfig =
-      typeof actionOrConfig === 'string' ? TOKEN_CONFIG_MAP[actionOrConfig] : actionOrConfig;
+    const tokenConfig = this.getTokenConfig(actionOrConfig);
 
     return jwt.sign(payload, tokenConfig.SECRET_KEY, {
       expiresIn: tokenConfig.LIFETIME,
     });
+  }
+
+  async verifyToken<T extends JwtPayload = JwtPayload>(
+    token: string,
+    action: SingleTokenActions,
+  ): Promise<T>;
+  async verifyToken<T extends JwtPayload = JwtPayload>(
+    token: string,
+    config: TokenConfig,
+  ): Promise<T>;
+  async verifyToken<T extends JwtPayload = JwtPayload>(
+    token: string,
+    actionOrConfig: SingleTokenActions | TokenConfig,
+  ): Promise<T> {
+    const tokenConfig = this.getTokenConfig(actionOrConfig);
+
+    const payload = (await jwt.verify(token, tokenConfig.SECRET_KEY)) as T;
+
+    return payload;
+  }
+
+  private getTokenConfig(actionOrConfig: SingleTokenActions | TokenConfig): TokenConfig {
+    return typeof actionOrConfig === 'string'
+      ? (TOKEN_CONFIG_MAP[actionOrConfig] as TokenConfig)
+      : actionOrConfig;
   }
 }
 
