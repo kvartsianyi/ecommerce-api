8eb2484c508adeb60aa74af7a8b63b9a35299785
Yuriy <liparg104@gmail.com>
2025-07-17 20:29:34 +0300

    fix: add body validation to /email-confirm endpoint

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 
 import { userController } from '@/controllers';
 import { validateBody, wrapAsyncErrors } from '@/middlewares';
-import { createUserSchema } from '@/validators';
+import { createUserSchema, tokenSchema } from '@/validators';
 
 const userRouter = Router();
 
 userRouter
   .post('/', validateBody(createUserSchema), wrapAsyncErrors(userController.createUser))
-  .post('/email-confirm', wrapAsyncErrors(userController.confirmEmail));
+  .post('/email-confirm', validateBody(tokenSchema), wrapAsyncErrors(userController.confirmEmail));
 
 export default userRouter;
