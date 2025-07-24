3f166143d5cc5f455d1791e4569a46c7e5d9204c
Yuriy Kvartsianiy <liparg104@gmail.com>
2025-07-24 11:47:38 +0300

    fix: remove controller wrapper, Express v5 auto-routes errors to error handler

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,3 @@
-export * from './async-handler.middleware';
 export * from './not-found.middleware';
 export * from './error-handler.middleware';
 export * from './validate-body.middleware';
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 
 import { userController } from '@/controllers';
-import { attachUserByEmail, validateBody, wrapAsyncErrors } from '@/middlewares';
+import { attachUserByEmail, validateBody } from '@/middlewares';
 import { createUserSchema, tokenSchema } from '@/validators';
 
 const userRouter = Router();
 
 userRouter
-  .post('/', validateBody(createUserSchema), wrapAsyncErrors(userController.createUser))
-  .post('/email-confirm', validateBody(tokenSchema), wrapAsyncErrors(userController.confirmEmail))
+  .post('/', validateBody(createUserSchema), userController.createUser)
+  .post('/email-confirm', validateBody(tokenSchema), userController.confirmEmail)
   .post('/email-confirm/send', attachUserByEmail, userController.sendConfirmEmail);
 
 export default userRouter;
