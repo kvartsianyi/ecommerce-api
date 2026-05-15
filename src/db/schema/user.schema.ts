444f8da54cc8ce4674ecec848e16681c0eb94b28
Yuriy <liparg104@gmail.com>
2026-05-15 23:45:15 +0300

    refactor: replace firstName and lastName with name field for user

--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -11,8 +11,7 @@ const users = pgTable(
   'users',
   {
     id: serial().primaryKey(),
-    firstName: varchar('first_name', { length: 50 }).notNull(),
-    lastName: varchar('last_name', { length: 50 }).notNull(),
+    name: varchar({ length: 50 }).notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
     phone: varchar({ length: 20 }).notNull().unique(),
     password: varchar({ length: 255 }).notNull(),
